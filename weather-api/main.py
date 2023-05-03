92cb717930168fdcd0e15821fffe6b51787d6421
Bubai Bal <bubaibal@amazon.com>
2023-05-03 11:56:03 +0530

    added more rest apis

--- weather-api/main.py
+++ weather-api/main.py
@@ -23,7 +23,7 @@ def about():
 # REST APIs
 @app.route("/api/v1/<station>/<date>")
 def restapi(station, date):
-    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt";
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt";
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {
@@ -31,6 +31,29 @@ def restapi(station, date):
         "date": date,
         "temperature": temperature
     }
+
+
+# Returns all the data of a station
+@app.route("/api/v1/<station>")
+def all_station_data(station):
+    filename = get_station_file(station)
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+# Returns data of a station by year
+@app.route("/api/v1/year/<station>/<year>")
+def yearly_station_data(station, year):
+    filename = get_station_file(station)
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
+def get_station_file(station):
+    return "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
 
 
 if __name__ == "__main__":
