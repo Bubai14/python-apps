36eb4063bd5ac22ac5b468dc75572fa2bce3bde2
Bubai Bal <bubaibal@amazon.com>
2023-05-04 12:19:02 +0530

    Added captions in the sky images

--- weather-api/main.py
+++ weather-api/main.py
@@ -23,7 +23,7 @@ def about():
 # REST APIs
 @app.route("/api/v1/<station>/<date>")
 def restapi(station, date):
-    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt";
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {
