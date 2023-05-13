4dbd5855b6c800d67dc13126bff443953426a09e
Bubai Bal <bubaibal@amazon.com>
2023-05-13 21:58:21 +0530

    Added OOP into the project

--- web-scapper/db.py
+++ web-scapper/db.py
@@ -1,31 +1,34 @@
 import sqlite3
 
-# Establish the database connection
-connection = sqlite3.connect("data.db")
 
+class Database:
 
-def get(row):
-    """
-    Queries the database with the row argument
-    :param row:
-    :return:
-        the row
-    """
-    # Get the specific columns from the row
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date = ?", (band, city, date))
-    return cursor.fetchall()
+    def __init__(self, database_file_path):
+        # Establish the database connection
+        # self is similar to this in Java and __init__ is the constructor in Java
+        self.connection = sqlite3.connect(database_file_path)
 
+    def get(self, row):
+        """
+        Queries the database with the row argument
+        :param row:
+        :return:
+            the row
+        """
+        # Get the specific columns from the row
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date = ?", (band, city, date))
+        return cursor.fetchall()
 
-def save(row):
-    """
-    Inserts the row to the database.
-    :param row:
-        the row to be inserted
-    :return:
-    """
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
-    connection.commit()
-    print("New tour added!!!")
+    def save(self, row):
+        """
+        Inserts the row to the database.
+        :param row:
+            the row to be inserted
+        :return:
+        """
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+        self.connection.commit()
+        print("New tour added!!!")
--- web-scapper/main.py
+++ web-scapper/main.py
@@ -4,59 +4,60 @@ import requests
 import selectorlib
 import db
 
-
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scape(url):
-    """
-    Scrape the page source from the URL
-    :param url:
-        The url to be scrapped
-    :return:
-        the scrapped page source
-    """
-    response = requests.get(url, headers=HEADERS)
-    page_source = response.text
-    return page_source
+class WebScrapper:
+    def scrape(self, url):
+        """
+        Scrape the page source from the URL
+        :param url:
+            The url to be scrapped
+        :return:
+            the scrapped page source
+        """
+        response = requests.get(url, headers=HEADERS)
+        page_source = response.text
+        return page_source
+
+    def extract(self, page_source):
+        """
+        Extracts the contents from the source
+        :param page_source:
+            The page source
+        :return:
+            The extracted content
+        """
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        return extractor.extract(page_source)['tours']
 
 
-def extract(page_source):
-    """
-    Extracts the contents from the source
-    :param page_source:
-        The page source
-    :return:
-        The extracted content
-    """
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    return extractor.extract(page_source)['tours']
+class Data:
+    def store(self, extracted):
+        """
+        Store the extracted value to the database
+        :param extracted:
+            extracted value
+        :return:
+        """
+        database = db.Database('data.db')
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        database.save(row)
 
-
-def store(extracted):
-    """
-    Store the extracted value to the database
-    :param extracted:
-        extracted value
-    :return:
-    """
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    db.save(row)
-
-
-def read(extracted):
-    """
-    Read the values from database
-    :return:
-        the tours list
-    """
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    return db.get(row)
+    def read(self, extracted):
+        """
+        Read the values from database
+        :return:
+            the tours list
+        """
+        database = db.Database('data.db')
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        return database.get(row)
 
 
 def message():
@@ -64,15 +65,14 @@ def message():
 
 
 if __name__ == "__main__":
+    web_scrapper = WebScrapper()
+    data = Data()
     while True:
-        source = scape(URL)
-        extracted = extract(source)
+        source = web_scrapper.scrape(URL)
+        extracted = web_scrapper.extract(source)
         if extracted != "No upcoming tours":
-            content = read(extracted)
+            content = data.read(extracted)
             if not content:
-                store(extracted)
+                data.store(extracted)
                 message()
         time.sleep(2)
-
-
-
