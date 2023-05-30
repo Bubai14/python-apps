9f0e6952e666ab240d2dab263fa6fdc9402865c0
Bubai Bal <bubaibal@amazon.com>
2023-05-30 11:26:25 +0530

    Added views and html files

--- django-restaurant-app/restaurant_menu/views.py
+++ django-restaurant-app/restaurant_menu/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Menu
+
 
 # Create your views here.
+class MenuList(generic.ListView):
+    queryset = Menu.objects.order_by('date_created')
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"]}
+        return context
+
+
+class Dish(generic.DetailView):
+    model = Menu
+    template_name = 'dish.html'
